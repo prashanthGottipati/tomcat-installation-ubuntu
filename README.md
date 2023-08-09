76588276f26b9a315cc1b317ab1aac3d06633c81
prashanthGottipati <prashanth.g@outlook.com>
2023-08-09 21:35:16 +0530

    Final changes

--- README.md
+++ README.md
@@ -5,7 +5,13 @@ Installation steps :
 
 1) Clone the repository to your ubuntu system or ubuntu server using -
        git clone https://github.com/prashanthGottipati/tomcat-installation-ubuntu.git
-2) Get inside the directory - 
+2) Get inside the directory -
        cd tomcat-installation-ubuntu
-3) And then install Tomcat using command - 
+3) And then install Tomcat using command -
        sh install-tomcat.sh
+4) After installation is successful proceed to open the browser -
+       http://<your_domain_or_IP_address>:8080. Change your_domain_or_IP_address.
+5) Credentials for Tomcat web app manager use  -
+       http://<your_domain_or_IP_address>:8080/manager/html
+       user - admin
+       password - password
--- install-tomcat.sh
+++ install-tomcat.sh
@@ -6,35 +6,36 @@ sudo apt update
 sudo apt install -y default-jdk wget
 
 # Download Tomcat
+cd /opt
 sudo rm -rf apache* tomcat*
-sudo cd /opt
-sudo mkdir tomcat
+sudo mkdir -p /opt/tomcat
 
 sudo wget -q https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.78/bin/apache-tomcat-9.0.78.tar.gz
 sudo tar -xf apache-tomcat-9.0.78.tar.gz  -C /opt/tomcat
 sudo rm -rf apache-tomcat-9.0.78.tar.gz
 
 # Configuring Tomcat server for Manager, Host-manager and Credentials
+cd -
 sudo cp context.xml /opt/tomcat/apache-tomcat-9.0.78/webapps/manager/META-INF/context.xml
 sudo cp context.xml /opt/tomcat/apache-tomcat-9.0.78/webapps/host-manager/META-INF/context.xml
 sudo cp tomcat-users.xml /opt/tomcat/apache-tomcat-9.0.78/conf/tomcat-users.xml
 
+# Configuring Tomcat as a Service
 sudo useradd -r -m -U -d /opt/tomcat -s /bin/false tomcat
 sudo chown -R tomcat: /opt/tomcat/*
-# Configuring Tomcat as a Service
 sudo cp tomcat.service /etc/systemd/system/tomcat.service
 sudo rm -rf tomcat-ubuntu
-
-sudo chmod +x /opt/tomcat/apache-tomcat-9.0.78/bin/*.sh
 
 sudo systemctl daemon-reload
 sudo systemctl start tomcat
 
 sudo ufw allow 8080
 
+#checking for Tomcat status
 sudo systemctl is-active --quiet tomcat
+
 echo "\n --------------------------------------------------------------------- \n"
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
     echo "Tomcat installed successfully"
     echo "Access Tomcat using http://$(curl -s ifconfig.me):8080"
 else
