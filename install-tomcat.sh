4e6417bff1b28e31ede74ecbb9b995b95c3809d5
prashanthGottipati <prashanth.g@outlook.com>
2023-08-09 16:24:15 +0530

    added changes

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -6,27 +6,24 @@ sudo apt update
 sudo apt install -y default-jdk wget
 
 # Download Tomcat
-cd /opt
-sudo systemctl stop tomcat
 sudo rm -rf apache* tomcat*
-sudo mkdir -p /opt/tomcat
+sudo cd /opt
+sudo mkdir tomcat
 
 sudo wget -q https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.78/bin/apache-tomcat-9.0.78.tar.gz
 sudo tar xvzf apache-tomcat-9.0.78.tar.gz
 sudo rm -rf apache*.tar.gz
 
+# Configuring Tomcat server for Manager, Host-manager and Credentials
+sudo cp context.xml /opt/tomcat/apache-tomcat-9.0.78/webapps/manager/META-INF/context.xml
+sudo cp context.xml /opt/tomcat/apache-tomcat-9.0.78/webapps/host-manager/META-INF/context.xml
+sudo cp tomcat-users.xml /opt/tomcat/apache-tomcat-9.0.78/conf/tomcat-users.xml
+# Configuring Tomcat as a Service
+sudo cp tomcat.service /etc/systemd/system/tomcat.service
+
 sudo useradd -r -m -U -d /opt/tomcat -s /bin/false tomcat
 sudo chown -R tomcat: /opt/tomcat/*
-# sudo chmod +x /opt/tomcat/apache-tomcat-9.0.78/bin/*.sh
-
-# Configuring Tomcat server for Manager, Host-manager and Credentials
-cd -
-sudo cp /context.xml /opt/tomcat/apache-tomcat-9.0.78/webapps/manager/META-INF/context.xml
-sudo cp /context.xml /opt/tomcat/apache-tomcat-9.0.78/webapps/host-manager/META-INF/context.xml
-sudo cp /tomcat-users.xml /opt/tomcat/apache-tomcat-9.0.78/conf/tomcat-users.xml
-# Configuring Tomcat as a Service
-sudo cp /tomcat.service /etc/systemd/system/tomcat.service
-
+sudo chmod +x /opt/tomcat/apache-tomcat-9.0.78/bin/*.sh
 
 sudo systemctl daemon-reload
 sudo systemctl start tomcat
